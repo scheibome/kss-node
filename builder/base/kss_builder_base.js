f929cff4082dd867cc7b6a707e7b3a74af034a9c
Felix Ranesberger <felix.ranesberger@in2code.de>
2024-10-04 09:38:23 +0200

    feat: work on migration

--- builder/base/kss_builder_base.js
+++ builder/base/kss_builder_base.js
@@ -639,12 +639,12 @@ class KssBuilderBase {
    * @returns {Promise} A promise to initialize the destination directory.
    */
   prepareDestination(assetDirectory) {
-    // check if directory exists, if yes delete
-    if (fs.existsSync(this.options.destination)) {
-      fs.removeSync(this.options.destination, {
-        recursive: true,
-      });
-    }
+    // // check if directory exists, if yes delete
+    // if (fs.existsSync(this.options.destination)) {
+    //   fs.removeSync(this.options.destination, {
+    //     recursive: true,
+    //   });
+    // }
 
     // Create a new destination directory.
     return fs.mkdir(this.options.destination).then(() => {
