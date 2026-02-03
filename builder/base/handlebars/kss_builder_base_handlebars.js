6f909d49fa1ec43d70e9a3245e2a946295eda04f
Felix Ranesberger <felix.ranesberger@in2code.de>
2026-02-03 10:23:55 +0100

    Revert "Merge pull request #2 from scheibome/feature/upgrade-glob"
    This reverts commit 13dfe6fb46d92c04d6c77c61dea6e9bc9701929b, reversing
    changes made to d668c6c534dd5f2befba900b0cb65d9edcfdcaa0.

--- builder/base/handlebars/kss_builder_base_handlebars.js
+++ builder/base/handlebars/kss_builder_base_handlebars.js
@@ -12,8 +12,10 @@
 const KssBuilderBase = require('../kss_builder_base.js'),
   path = require('path'),
   pug = require('pug'),
-  fs = require('fs-extra'),
+  Promise = require('bluebird'),
   Handlebars = require('handlebars');
+
+const fs = Promise.promisifyAll(require('fs-extra'));
 
 /**
  * A kss-node builder takes input files and builds a style guide using
@@ -82,13 +84,13 @@ class KssBuilderBaseHandlebars extends KssBuilderBase {
     let options = {};
     // Returns a promise to read/load a template provided by the builder.
     options.readBuilderTemplate = (name) => {
-      return fs.readFile(path.resolve(this.options.builder, name + '.hbs'), 'utf8').then(content => {
+      return fs.readFileAsync(path.resolve(this.options.builder, name + '.hbs'), 'utf8').then(content => {
         return this.Handlebars.compile(content);
       });
     };
     // Returns a promise to read/load a template specified by a section.
     options.readSectionTemplate = (name, filepath) => {
-      return fs.readFile(filepath, 'utf8').then(fileContent => {
+      return fs.readFileAsync(filepath, 'utf8').then(fileContent => {
         let output = fileContent;
 
         const isPugFile = path.extname(filepath) === '.pug';
